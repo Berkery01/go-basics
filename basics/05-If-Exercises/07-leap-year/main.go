c949dd1a1fa831dca1afa080375b958201587c48
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-20 15:42:55 +0300

    OK.

--- basics/05-If-Exercises/07-leap-year/main.go
+++ basics/05-If-Exercises/07-leap-year/main.go
@@ -54,7 +54,7 @@ func main() {
 		fmt.Printf("%q is not a year number.\n", os.Args[1])
 		return
 	}
-	if (year%4 == 0 && year%100 == 0 && year%400 == 0) || (year%4 == 0 && year%100 != 0) {
+	if (year%400 == 0) || (year%4 == 0 && year%100 != 0) {
 		fmt.Printf("%d is a leap year.\n", year)
 	} else {
 		fmt.Printf("%d is not a leap year.\n", year)
