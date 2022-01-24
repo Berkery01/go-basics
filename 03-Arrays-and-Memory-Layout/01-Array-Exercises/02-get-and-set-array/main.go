43b71fbc8f960d1c049bd0966d8179b2779b2b95
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-24 15:57:24 +0300

    OK.

---
+++ 03-Arrays-and-Memory-Layout/01-Array-Exercises/02-get-and-set-array/main.go
@@ -0,0 +1,165 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Get and Set Array Elements
+//
+//  1. Use the 01-declare-empty exercise
+//  2. Remove everything but the array declarations
+//
+//  3. Assign your best friends' names to the names array
+//
+//  4. Assign distances to the closest cities to you to the distance array
+//
+//  5. Assign arbitrary bytes to the data array
+//
+//  6. Assign a value to the ratios array
+//
+//  7. Assign true/false values to the alives arrays
+//
+//  8. Try to assign to the zero array and observe the error
+//
+//  9. Now use ordinary loop statements for each array and print them
+//      (do not use for range)
+//
+//  10. Now use for range loop statements for each array and print them
+//
+//  11. Try assigning different types of values to the arrays, break things,
+//     and observe the errors
+//
+//  12. Remove some of the array assignments and observe the loop outputs
+//      (zero values)
+//
+//
+// EXPECTED OUTPUT
+//
+//  Note: The output can change depending on the values that you've assigned to them, of course.
+//        You're free to assign any values.
+//
+//   names
+//   ====================
+//   names[0]: "Einstein"
+//   names[1]: "Tesla"
+//   names[2]: "Shepard"
+//
+//   distances
+//   ====================
+//   distances[0]: 50
+//   distances[1]: 40
+//   distances[2]: 75
+//   distances[3]: 30
+//   distances[4]: 125
+//
+//   data
+//   ====================
+//   data[0]: 72
+//   data[1]: 69
+//   data[2]: 76
+//   data[3]: 76
+//   data[4]: 79
+//
+//   ratios
+//   ====================
+//   ratios[0]: 3.14
+//
+//   alives
+//   ====================
+//   alives[0]: true
+//   alives[1]: false
+//   alives[2]: true
+//   alives[3]: false
+//
+//   zero
+//   ====================
+
+//
+//   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   FOR RANGES
+//   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//   names
+//   ====================
+//   names[0]: "Einstein"
+//   names[1]: "Tesla"
+//   names[2]: "Shepard"
+//
+//   distances
+//   ====================
+//   distances[0]: 50
+//   distances[1]: 40
+//   distances[2]: 75
+//   distances[3]: 30
+//   distances[4]: 125
+//
+//   data
+//   ====================
+//   data[0]: 72
+//   data[1]: 69
+//   data[2]: 76
+//   data[3]: 76
+//   data[4]: 79
+//
+//   ratios
+//   ====================
+//   ratios[0]: 3.14
+//
+//   alives
+//   ====================
+//   alives[0]: true
+//   alives[1]: false
+//   alives[2]: true
+//   alives[3]: false
+//
+//   zero
+//   ====================
+//
+// ---------------------------------------------------------
+
+func main() {
+
+	var (
+		names     [3]string
+		distances [5]int
+		data      [5]uint8
+		ratios    [1]float64
+		alives    [4]bool
+		zero      [0]uint8
+	)
+
+	names[0] = "A"
+	names[1] = "B"
+	names[2] = "C"
+
+	distances[0] = 100
+	distances[1] = 200
+	distances[2] = 300
+	distances[3] = 400
+	distances[4] = 500
+
+	data[0] = 12
+	data[1] = 12
+	data[2] = 12
+	data[3] = 12
+	data[4] = 12
+
+	ratios[0] = 1.42
+
+	alives[0] = true
+	alives[len(alives)-1] = true
+
+	fmt.Printf("names     : %q\n", names)
+	fmt.Printf("distances : %d\n", distances)
+	fmt.Printf("data      : %d\n", data)
+	fmt.Printf("ratios    : %.2f\n", ratios)
+	fmt.Printf("alives    : %t\n", alives)
+	fmt.Printf("zero      : %d\n", zero)
+}
