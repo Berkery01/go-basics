c5e0c11261a15aaebbeeae0bed5bae995b60a813
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-25 13:14:53 +0300

    OK.

---
+++ 03-Arrays-and-Memory-Layout/02-Array-Exercises-2/05-sorter/main.go
@@ -0,0 +1,79 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Number Sorter
+//
+//  Your goal is to sort the given numbers from the command-line.
+//
+//  1. Get the numbers from the command-line.
+//
+//  2. Create an array and assign the given numbers to that array.
+//
+//  3. Sort the given numbers and print them.
+//
+// RESTRICTION
+//   + Maximum 5 numbers can be provided
+//   + If one of the arguments is not a valid number, skip it
+//
+// HINTS
+//  + You can use the bubble-sort algorithm to sort the numbers.
+//    Please watch this: https://youtu.be/nmhjrI-aW5o?t=7
+//
+//  + When swapping the elements, do not check for the last element.
+//
+//    Or, you will receive this error:
+//    "panic: runtime error: index out of range"
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please give me up to 5 numbers.
+//
+//   go run main.go 6 5 4 3 2 1
+//     Sorry. Go arrays are fixed. So, for now, I'm only supporting sorting 5 numbers...
+//
+//   go run main.go 5 4 3 2 1
+//     [1 2 3 4 5]
+//
+//   go run main.go 5 4 a c 1
+//     [0 0 1 4 5]
+// ---------------------------------------------------------
+
+func main() {
+	var (
+		input  [5]float64
+		sorted [5]float64
+	)
+
+	for i, v := range os.Args[1:] {
+		num, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			//fmt.Printf("Error: %q\n", err)
+			continue
+		}
+		input[i] = num
+	}
+
+	sorted = input
+	for i := len(sorted); i > 0; i-- {
+		for j := 0; j < i-1; j++ {
+			if sorted[j] > sorted[j+1] {
+				sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
+			}
+		}
+	}
+	fmt.Printf("%v\n", sorted)
+}
