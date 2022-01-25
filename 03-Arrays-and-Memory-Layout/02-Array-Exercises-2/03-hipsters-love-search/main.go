5b1ad40de93df2e5fd04f724b2393f8a244e79d1
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-25 12:39:54 +0300

    OK.

---
+++ 03-Arrays-and-Memory-Layout/02-Array-Exercises-2/03-hipsters-love-search/main.go
@@ -0,0 +1,100 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Hipster's Love Bookstore Search Engine
+//
+//  Your goal is to allow people to search for books.
+//
+//  1. Create an array with the following book titles:
+//      Kafka's Revenge
+//      Stay Golden
+//      Everythingship
+//      Kafka's Revenge 2nd Edition
+//
+//  2. Get the search query from the command-line argument
+//
+//  3. Search for the books in the books array
+//
+//  4. When the program finds the book, print it.
+//  5. Otherwise, print that the book doesn't exist.
+//
+//  6. Handle the errors.
+//
+// RESTRICTION:
+//   + The search should be case insensitive.
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Tell me a book title
+//
+//   go run main.go STAY
+//     Search Results:
+//     + Stay Golden
+//
+//   go run main.go sTaY
+//     Search Results:
+//     + Stay Golden
+//
+//   go run main.go "Kafka's Revenge"
+//     Search Results:
+//     + Kafka's Revenge
+//     + Kafka's Revenge 2nd Edition
+//
+//   go run main.go void
+//     Search Results:
+//     We don't have the book: "void"
+//
+// HINTS:
+//   + To find out whether a string contains another string value, you can use the strings.Contains function.
+//   + To convert a string value to lowercase, you can use the strings.ToLower function.
+//   + Check out the strings package for more information.
+// ---------------------------------------------------------
+
+func main() {
+	books := [...]string{
+		"Kafka's Revenge",
+		"Stay Golden",
+		"Everythingship",
+		"Kafka's Revenge 2nd Edition",
+	}
+	notFound := true
+	var isFound [4]bool
+
+	if len(os.Args) != 2 {
+		fmt.Println("Tell me a book name.")
+		return
+	}
+
+	for i := range books {
+		if strings.Contains(strings.ToLower(books[i]), strings.ToLower(os.Args[1])) {
+			isFound[i] = true
+			notFound = false
+		}
+	}
+
+	if notFound {
+		fmt.Printf("We don't have book: %q\n", os.Args[1])
+	} else {
+		fmt.Println("Search result:")
+		for i, v := range isFound {
+			if v {
+				fmt.Printf("+ %s\n", books[i])
+			}
+		}
+	}
+
+}
