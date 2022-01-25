349e1c9bd5ddc95844f787f9454964a06bb29515
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-25 10:54:52 +0300

    OK.

---
+++ 03-Arrays-and-Memory-Layout/02-Array-Exercises-2/01-wizard-printer/main.go
@@ -0,0 +1,55 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Wizard Printer
+//
+//   In this exercise, your goal is to display a few famous scientists
+//   in a pretty table.
+//
+//   1. Create a multi-dimensional array
+//   2. In each inner array, store the scientist's name, lastname and his/her
+//      nickname
+//   3. Print their information in a pretty table using a loop.
+//
+// EXPECTED OUTPUT
+//   First Name      Last Name       Nickname
+//   ==================================================
+//   Albert          Einstein        time
+//   Isaac           Newton          apple
+//   Stephen         Hawking         blackhole
+//   Marie           Curie           radium
+//   Charles         Darwin          fittest
+// ---------------------------------------------------------
+
+func main() {
+
+	sci := [...][3]string{
+		{"First Name", "Last Name", "Nickname"},
+		{"Albert", "Einstein", "time"},
+		{"Isaac", "Newton", "apple"},
+		{"Stephen", "Hawking", "blackhole"},
+		{"Marie", "Curie", "radium"},
+		{"Charles", "Darwin", "fittest"},
+	}
+
+	for i := range sci {
+		fmt.Printf("%-10s %-10s %-10s\n", sci[i][0], sci[i][1], sci[i][2])
+		if i == 0 {
+			fmt.Println(strings.Repeat("=", 40))
+		}
+	}
+
+}
