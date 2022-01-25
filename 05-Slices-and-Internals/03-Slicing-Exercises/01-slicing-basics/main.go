b4ab11e287095e164d6824b02a0b6d93e5018b40
Berkery01 <byuruktumen18@ku.edu.tr>
2022-01-25 17:56:33 +0300

    OK.

---
+++ 05-Slices-and-Internals/03-Slicing-Exercises/01-slicing-basics/main.go
@@ -0,0 +1,80 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Slice the numbers
+//
+//   We've a string that contains even and odd numbers.
+//
+//   1. Convert the string to an []int
+//
+//   2. Print the slice
+//
+//   3. Slice it for the even numbers and print it (assign it to a new slice variable)
+//
+//   4. Slice it for the odd numbers and print it (assign it to a new slice variable)
+//
+//   5. Slice it for the two numbers at the middle
+//
+//   6. Slice it for the first two numbers
+//
+//   7. Slice it for the last two numbers (use the len function)
+//
+//   8. Slice the evens slice for the last one number
+//
+//   9. Slice the odds slice for the last two numbers
+//
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//
+//     nums        : [2 4 6 1 3 5]
+//     evens       : [2 4 6]
+//     odds        : [1 3 5]
+//     middle      : [6 1]
+//     first 2     : [2 4]
+//     last 2      : [3 5]
+//     evens last 1: [6]
+//     odds last 2 : [3 5]
+//
+//
+// NOTE
+//
+//  You can also use my prettyslice package for printing the slices.
+//
+//
+// HINT
+//
+//  Find a function in the strings package for splitting the string into []string
+//
+// ---------------------------------------------------------
+
+func main() {
+	// uncomment the declaration below
+	data := "2 4 6 1 3 5"
+
+	const separator = " "
+
+	nums := strings.Split(data, separator)
+	l := len(nums)
+	fmt.Printf("Nums            :%v\n", nums)
+	fmt.Printf("Evens           :%v\n", nums[:3])
+	fmt.Printf("Odds            :%v\n", nums[3:])
+	fmt.Printf("Mid             :%v\n", nums[2:4])
+	fmt.Printf("First 2         :%v\n", nums[:2])
+	fmt.Printf("Last 2          :%v\n", nums[l-2:])
+	fmt.Printf("Evens last 1    :%v\n", nums[:3][2:])
+	fmt.Printf("Odds last 2     :%v\n", nums[3:][1:])
+}
